0b7fcd2a7dc991f8d3d38da79aec66fec631f9e5
Todd Wahnish <t.wahnish@gmail.com>
2013-02-27 21:26:39 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookReviewApp::Application.config.secret_token = '86dfb4890962e8fb7fbf0de47e29716891aac785dbe864baa92a840e1154439bf68d60e031d38526e29fbc05806509c69681d2db59e01bef09a91d4b08873a7e'
